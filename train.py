05a2d52dbc621d9230808a8eb4990154ea757397
Hyeongseok Jeon <521hsgugu@kaist.ac.kr>
2021-12-21 00:12:59 +0900

    sadf

--- train.py
+++ train.py
@@ -329,4 +329,4 @@ plt.axis("off")
 ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
 anim = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 anim.save('infoGAN_{}.gif'.format(params['dataset']), dpi=80, writer='imagemagick')
-plt.show()
+# plt.show()
