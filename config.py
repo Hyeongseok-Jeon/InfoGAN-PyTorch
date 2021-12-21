324d26eadab628479c9e23b16764c30c881ae83f
Hyeongseok Jeon <521hsgugu@kaist.ac.kr>
2021-12-21 16:48:44 +0900

    asdf

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 params = {
     'batch_size': 128,# Batch size.
     'num_epochs': 500,# Number of epochs to train for.
-    'learning_rate_D': 2e-4,# Learning rate.
-    'learning_rate_G': 1e-4,# Learning rate.
+    'learning_rate_D': 1e-4,# Learning rate.
+    'learning_rate_G': 2e-4,# Learning rate.
     'beta1': 0.5,
     'beta2': 0.999,
     'save_epoch' : 25,# After how many epochs to save checkpoints and generate test output.
